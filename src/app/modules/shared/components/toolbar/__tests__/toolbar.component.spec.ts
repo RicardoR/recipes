2c132657dc32dc7a0c0bcdc6df868a3ef6886ab3
Ricardo <rirova81@gmail.com>
2021-11-07 22:48:51 +0100

    WIP components UT

--- src/app/modules/shared/components/toolbar/__tests__/toolbar.component.spec.ts
+++ src/app/modules/shared/components/toolbar/__tests__/toolbar.component.spec.ts
@@ -1,15 +1,25 @@
-/* tslint:disable:no-unused-variable */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
 import { ToolbarComponent } from '../toolbar.component';
+import { userMocked } from './user-mocked';
 
-fdescribe('ToolbarComponent', () => {
+describe('ToolbarComponent', () => {
   let component: ToolbarComponent;
   let fixture: ComponentFixture<ToolbarComponent>;
+  const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+  const authServiceSpy = jasmine.createSpyObj('AuthService', [
+    'currentUser',
+    'logout',
+  ]);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ToolbarComponent]
+      declarations: [ToolbarComponent],
+      providers: [
+        { provide: Router, useValue: routerSpy },
+        { provide: AuthService, useValue: authServiceSpy }
+      ]
     })
       .overrideTemplate(ToolbarComponent, '');
   });
@@ -17,10 +27,31 @@ fdescribe('ToolbarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ToolbarComponent);
     component = fixture.componentInstance;
+    authServiceSpy.currentUser = userMocked;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should get the userId from authService', () => {
+    expect(component.userId).toEqual(userMocked.uid);
   });
+
+  it('goToCreate should navigate to recipes/new', () => {
+    component.goToCreate();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['recipes/new']);
+  });
+
+  it('goToPrivateList should navigate to the private list', () => {
+    component.goToPrivateList();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['recipes/my-recipes']);
+  });
+
+  it('goToPublicList should navigate to recipes', () => {
+    component.goToPublicList();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['recipes']);
+  });
+
+  it('logout should call to logout method from authService', () => {
+    component.logout();
+    expect(authServiceSpy.logout).toHaveBeenCalled();
+  })
 });
---
+++ src/app/modules/shared/components/toolbar/__tests__/user-mocked.ts
@@ -0,0 +1,7 @@
+import { AuthData } from '../../../../auth/auth-data.model';
+
+export const userMocked: AuthData = {
+  uid: '2',
+  email: 'email@domain.com',
+  password: '123',
+}
