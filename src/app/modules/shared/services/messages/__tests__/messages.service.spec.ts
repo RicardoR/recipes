1f46e5816eeb389573970f1c8f49eaf60a55f64b
Ricardo <ricardo.rv@outlook.com>
2023-09-25 18:49:38 +0200

    fix: ut and font

--- src/app/modules/shared/services/messages/__tests__/messages.service.spec.ts
+++ src/app/modules/shared/services/messages/__tests__/messages.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { MatLegacySnackBar as MatSnackBar } from '@angular/material/legacy-snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { MessagesService } from '../messages.service';
 
