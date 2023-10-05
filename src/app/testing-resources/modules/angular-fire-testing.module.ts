06908eebc0e77c19842fc91bea1e344729efd8c8
Ricardo <ricardo.rv@outlook.com>
2023-10-05 20:09:12 +0200

    test: create a AngularFireTestingModule

---
+++ src/app/testing-resources/modules/angular-fire-testing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAnalytics, AngularFireAnalyticsModule } from "@angular/fire/compat/analytics";
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+import { environment } from "src/environments/environment";
+
+const mockedPromise: Promise<void> = new Promise(() => {});
+
+@NgModule({
+  imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAnalyticsModule,
+  ],
+})
+export class AngularFireTestingModule {
+  static getAngularFireAnalyticsSpy(): jasmine.SpyObj<any> {
+    return spyOn(AngularFireAnalytics.prototype, 'logEvent').and.returnValue(mockedPromise);
+  }
+}
