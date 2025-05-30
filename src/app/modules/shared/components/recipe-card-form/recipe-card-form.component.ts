3d3b954008e1b4fe9f99bf83345d10f6464d9b0b
Ricardo <rirova81@gmail.com>
2025-05-30 19:45:41 +0200

    Update to angular 17 (#44)
    * chore: bump zone.js and typescript
    
    * chore: bump to angular 17
    
    * chore: use ngxmc/file-input
    
    * fix: styles in recipe list
    
    * fix: styles in recipe list
    
    * fix: disable file input controller
    
    * fix: disable file input controller

--- src/app/modules/shared/components/recipe-card-form/recipe-card-form.component.ts
+++ src/app/modules/shared/components/recipe-card-form/recipe-card-form.component.ts
@@ -33,13 +33,13 @@ import { RecipeService } from 'src/app/modules/recipes/services/recipe/recipe.se
 import { MessagesService } from '../../services/messages/messages.service';
 import { UtilService } from '../../utils/utils.service';
 import { NgLog } from '../../utils/decorators/log-decorator';
-import { ElementModel } from './../../../recipes/models/element.model';
+import { ElementModel } from '../../../recipes/models/element.model';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RecipesMultipleSelectComponent } from '../recipes-multiple-select/recipes-multiple-select.component';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { NgxMatFileInputModule } from '@angular-material-components/file-input';
+//import { NgxMatFileInputComponent } from '@ngxmc/file-input';
 import { NgIf, NgFor, AsyncPipe } from '@angular/common';
 import { TextFieldModule } from '@angular/cdk/text-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -63,7 +63,7 @@ export const MEDIA_STORAGE_PATH = `recipes/images`;
     MatIconModule,
     TextFieldModule,
     NgIf,
-    NgxMatFileInputModule,
+    //NgxMatFileInputComponent,
     MatProgressBarModule,
     MatButtonModule,
     MatExpansionModule,
