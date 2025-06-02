21d60c773bf42e62ee05bc7f8dac43464fea197a
Ricardo <ricardo.rv@outlook.com>
2025-06-02 21:06:17 +0200

    chore: use provide auth and remove AngularFireModule.initializeApp(environment.firebase)

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAnalytics, provideAnalytics } from '@angular/fire/analytics';
-import { AuthModule } from './app/modules/auth/auth.module';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireModule } from '@angular/fire/compat';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { AppErrorHandler } from './app/app-error-handle';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { getAuth, provideAuth } from "@angular/fire/auth";
+
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppErrorHandler } from './app/app-error-handle';
 
 
 if (environment.production) {
@@ -24,17 +24,14 @@ bootstrapApplication(AppComponent, {
       provideFirebaseApp(() => initializeApp(environment.firebase)),
       provideFirestore(() => getFirestore()),
       provideAnalytics(() => getAnalytics()),
+      provideAuth(() => getAuth()),
+      { provide: FIREBASE_OPTIONS, useValue: environment.firebase },
       importProvidersFrom(
         BrowserModule,
         AppRoutingModule,
-        // Firebase
-        AngularFireModule.initializeApp(environment.firebase), // todo: can be removed ?
-        AuthModule,
         MatSnackBarModule,
         ServiceWorkerModule.register('ngsw-worker.js', {
             enabled: environment.production,
-            // Register the ServiceWorker as soon as the app is stable
-            // or after 30 seconds (whichever comes first).
             registrationStrategy: 'registerWhenStable:30000'
           })),
           {
