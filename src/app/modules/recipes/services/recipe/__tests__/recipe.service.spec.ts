3f341b7e3436639a05b67253e9415111e72881c6
Ricardo <ricardo.rv@outlook.com>
2025-06-14 13:54:31 +0200

    ✅ test: add unit tests for RecipeService methods including getOwnRecipes, getCategories, getPublicRecipes, and cloneRecipe

--- src/app/modules/recipes/services/recipe/__tests__/recipe.service.spec.ts
+++ src/app/modules/recipes/services/recipe/__tests__/recipe.service.spec.ts
@@ -7,6 +7,7 @@ import {recipeMock} from 'src/app/testing-resources/mocks/recipe-mock';
 import {userMock} from 'src/app/testing-resources/mocks/user-mock';
 import {Firestore} from '@angular/fire/firestore';
 import {Storage} from '@angular/fire/storage';
+import {ElementModel} from '../../../models/element.model';
 
 describe('RecipeService', () => {
   let service: RecipeService;
@@ -84,6 +85,43 @@ describe('RecipeService', () => {
       const filtered = service.filterRecipes(recipes, recipeMock.title);
       expect(filtered.length).toBe(1);
     });
+
+    it('should get own recipes', (done) => {
+      const recipes = [recipeMock];
+      spyOn(service, 'getOwnRecipes').and.returnValue(of(recipes));
+      service.getOwnRecipes().subscribe((res) => {
+        expect(res.length).toBe(1);
+        done();
+      });
+    });
+
+    it('should get categories', (done) => {
+      const categories: ElementModel[] = [{ id: 1, detail: 'Category 1' }];
+      spyOn(service, 'getCategories').and.returnValue(of(categories));
+      service.getCategories().subscribe((res) => {
+        expect(res.length).toBe(1);
+        expect(res[0].id).toBe(1);
+        done();
+      });
+    });
+
+    it('should get public recipes', (done) => {
+      const recipes = [recipeMock];
+      spyOn(service, 'getPublicRecipes').and.returnValue(of(recipes));
+      service.getPublicRecipes().subscribe((res) => {
+        expect(res.length).toBe(1);
+        done();
+      });
+    });
+
+    it('should clone a recipe', (done) => {
+      const cloneId = 'cloned123';
+      spyOn(service, 'cloneRecipe').and.returnValue(of(cloneId));
+      service.cloneRecipe(recipeMock).subscribe((id) => {
+        expect(id).toBe(cloneId);
+        done();
+      });
+    });
   });
 
   describe('when user is a demo user', () => {
@@ -109,6 +147,10 @@ describe('RecipeService', () => {
 
     it('should not delete image', () => {
       expect(() => service.deleteImage('someurl')).toThrow();
+    });
+
+    it('should not clone a recipe', () => {
+      expect(() => service.cloneRecipe(recipeMock)).toThrow();
     });
   });
 });
