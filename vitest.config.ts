d9de56b98dec660ee5d0ccef52cd04c4db440049
Ricardo <ricardo.rv@outlook.com>
2026-02-05 20:44:30 +0100

    chore: update test configuration for Vitest and add coverage settings

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/test.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'src/test.ts',
+      ]
+    }
+  }
+});
